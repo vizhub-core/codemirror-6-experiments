9c93cbcb9dce65e2ecec8e94e8bfcb6f89be6fca
curran <curran.kelleher@gmail.com>
2018-10-24 18:17:18 +0530

    Refactor indentation keymap

--- demo/demo.ts
+++ demo/demo.ts
@@ -10,10 +10,18 @@ import {
   matchBrackets,
   javascript,
   specialChars,
-  multipleSelections,
-  crudeInsertNewlineAndIndent,
-  crudeIndentLine
+  multipleSelections
 } from './codemirror';
+
+import { indentationKeymap } from './indentationKeymap';
+
+const historyKeymap = {
+  "Mod-z": undo,
+  "Mod-Shift-z": redo,
+  "Mod-u": view => undoSelection(view) || true,
+  [isMac ? "Mod-Shift-u" : "Alt-u"]: redoSelection,
+  "Ctrl-y": isMac ? undefined : redo
+};
 
 let mode = legacyMode(javascript({indentUnit: 2}, {}))
 let isMac = /Mac/.test(navigator.platform)
@@ -29,16 +37,9 @@ readFile("package.json", "utf8", (err, data) => {
   multipleSelections(),
   mode,
   matchBrackets({decorationsPlugin: mode}),
-  keymap({
-    "Mod-z": undo,
-    "Mod-Shift-z": redo,
-    "Mod-u": view => undoSelection(view) || true,
-    [isMac ? "Mod-Shift-u" : "Alt-u"]: redoSelection,
-    "Ctrl-y": isMac ? undefined : redo,
-    "Enter": crudeInsertNewlineAndIndent,
-    "Shift-Tab": crudeIndentLine
-  }),
-  keymap(baseKeymap),
+  keymap(historyKeymap),
+  keymap(indentationKeymap(mode)),
+  keymap(baseKeymap)
 ]})
 
 let view = (window as any).view = new EditorView(state)
---
+++ demo/indentationKeymap.ts
@@ -0,0 +1,25 @@
+export const indentationKeymap = mode => {
+  function crudeInsertNewlineAndIndent({state, dispatch}: EditorView): boolean {
+    let indentation = (mode as any).indentation(state, state.selection.primary.from)
+    if (indentation > -1)
+      dispatch(state.transaction.replaceSelection("\n" + " ".repeat(indentation)).scrollIntoView())
+    return true
+  }
+
+  function crudeIndentLine({state, dispatch}: EditorView): boolean {
+    let cursor = state.selection.primary.head // FIXME doesn't indent multiple lines
+    let line = state.doc.lineAt(cursor), text = line.slice(0, 100)
+    let space = /^ */.exec(text)[0].length // FIXME doesn't handle tabs
+    let indentation = (mode as any).indentation(state, line.start)
+    if (indentation == -1) indentation = space
+    let tr = state.transaction.replace(line.start, line.start + space, " ".repeat(indentation)).scrollIntoView()
+    if (cursor <= line.start + space)
+      tr = tr.setSelection(EditorSelection.single(line.start + indentation))
+    dispatch(tr)
+    return true
+  }
+  return {
+    "Enter": crudeInsertNewlineAndIndent,
+    "Shift-Tab": crudeIndentLine
+  };
+};
