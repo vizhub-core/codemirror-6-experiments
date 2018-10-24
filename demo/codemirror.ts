fc7cf5bfd65eb7fa771aa3f5d92d956025e451c1
curran <curran.kelleher@gmail.com>
2018-10-24 18:00:23 +0530

    Refactor indentation logic out of demo

--- demo/codemirror.ts
+++ demo/codemirror.ts
@@ -11,3 +11,23 @@ export {multipleSelections} from "../codemirror.next/multiple-selections/src/mul
 
 import javascript from "../codemirror.next/legacy-modes/src/javascript"
 export { javascript }
+
+export function crudeInsertNewlineAndIndent({state, dispatch}: EditorView): boolean {
+  let indentation = (mode as any).indentation(state, state.selection.primary.from)
+  if (indentation > -1)
+    dispatch(state.transaction.replaceSelection("\n" + " ".repeat(indentation)).scrollIntoView())
+  return true
+}
+
+export function crudeIndentLine({state, dispatch}: EditorView): boolean {
+  let cursor = state.selection.primary.head // FIXME doesn't indent multiple lines
+  let line = state.doc.lineAt(cursor), text = line.slice(0, 100)
+  let space = /^ */.exec(text)[0].length // FIXME doesn't handle tabs
+  let indentation = (mode as any).indentation(state, line.start)
+  if (indentation == -1) indentation = space
+  let tr = state.transaction.replace(line.start, line.start + space, " ".repeat(indentation)).scrollIntoView()
+  if (cursor <= line.start + space)
+    tr = tr.setSelection(EditorSelection.single(line.start + indentation))
+  dispatch(tr)
+  return true
+}
--- demo/demo.ts
+++ demo/demo.ts
@@ -10,33 +10,12 @@ import {
   matchBrackets,
   javascript,
   specialChars,
-  multipleSelections
+  multipleSelections,
+  crudeInsertNewlineAndIndent,
+  crudeIndentLine
 } from './codemirror';
 
 let mode = legacyMode(javascript({indentUnit: 2}, {}))
-
-// FIXME these should move to commands and access the indentation
-// feature through some kind of generic mechanism that allows plugins
-// to advertise that they can do indentation
-function crudeInsertNewlineAndIndent({state, dispatch}: EditorView): boolean {
-  let indentation = (mode as any).indentation(state, state.selection.primary.from)
-  if (indentation > -1)
-    dispatch(state.transaction.replaceSelection("\n" + " ".repeat(indentation)).scrollIntoView())
-  return true
-}
-function crudeIndentLine({state, dispatch}: EditorView): boolean {
-  let cursor = state.selection.primary.head // FIXME doesn't indent multiple lines
-  let line = state.doc.lineAt(cursor), text = line.slice(0, 100)
-  let space = /^ */.exec(text)[0].length // FIXME doesn't handle tabs
-  let indentation = (mode as any).indentation(state, line.start)
-  if (indentation == -1) indentation = space
-  let tr = state.transaction.replace(line.start, line.start + space, " ".repeat(indentation)).scrollIntoView()
-  if (cursor <= line.start + space)
-    tr = tr.setSelection(EditorSelection.single(line.start + indentation))
-  dispatch(tr)
-  return true
-}
-
 let isMac = /Mac/.test(navigator.platform)
 let state = EditorState.create({doc: `"use strict";
 const {readFile} = require("fs");
