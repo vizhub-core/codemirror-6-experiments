4266bd28a5de4ff8cff81c3592afdd2eaeb6dffb
curran <curran.kelleher@gmail.com>
2018-10-25 13:24:07 +0530

    Dispatch transaction programmatically

--- demo/experimentPlugin.ts
+++ demo/experimentPlugin.ts
@@ -1,14 +1,21 @@
 import { Plugin, EditorView, EditorState, Transaction } from './codemirror';
 
 export const experimentPlugin = new Plugin({
-  // Goal #1: Listen for all transactions,
-  // so they can be converted to OT operations.
-  view: (view: EditorView) => ({
-    updateState(view: EditorView, prev: EditorState, trs: Transaction[]) {
-      console.log(trs);
-    }
+  view: (view: EditorView) => {
+
+    // Inject programmatically created transactions,
+    // from remote OT operations.
+    setTimeout(() => {
+      console.log('dispatching');
+      view.dispatch(view.state.transaction.replace(1, 2, "*"))
+    }, 2000);
+
+    return {
+      // Listen for all transactions,
+      // so they can be converted to OT operations.
+      updateState(view: EditorView, prev: EditorState, trs: Transaction[]) {
+        console.log(trs);
+      }
+    };
   })
-  //
-  // Goal #2: Inject programmatically created transactions,
-  // from remote OT operations.
 });
