1c5ff6551531a78c9ecd43f5059a2e5b37693dc6
curran <curran.kelleher@gmail.com>
2018-11-01 01:13:24 +0530

    Render HTML from JSDOM in server

--- packages/experiments/demo/server.ts
+++ packages/experiments/demo/server.ts
@@ -1,17 +1,19 @@
 import * as express  from 'express';
-import {
-  Application,
-  Router,
-  Request,
-  Response
-} from 'express';
+import { Application, Router, Request, Response } from 'express';
+import * as jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+const html = '<!DOCTYPE html><p>Hello JSDOM</p>'
+
+const document = new JSDOM(html).window.document;
+//global.document = new JSDOM(html).window.document;
 
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  const html = document.body.outerHTML;
+  res.send(html);
 });
-
 
 const app: Application = express();
 const port: number = 3000;
