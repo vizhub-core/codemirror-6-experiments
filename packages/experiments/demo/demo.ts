6d5665ea1516298036733d1dc400e87e458b50b7
curran <curran.kelleher@gmail.com>
2018-11-01 20:55:46 +0530

    Adopt rollup-plugin-postcss

--- packages/experiments/demo/demo.ts
+++ packages/experiments/demo/demo.ts
@@ -1,3 +1,6 @@
 import { createView } from './demoView';
+import './styles.css';
+import 'codemirror-6/codemirror.next/view/style/editorview.css';
+import 'codemirror-theme-ubuntu/codemirror-ubuntu-theme.css';
 const view = createView();
 document.querySelector("#editor").appendChild(view.dom)
