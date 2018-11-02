40a43dfb5a5530c3ce54c56dc3adcc7a707df3aa
curran <curran.kelleher@gmail.com>
2018-11-03 01:06:51 +0530

    Subscribe to doc

--- packages/experiments/demo/client/demo.ts
+++ packages/experiments/demo/client/demo.ts
@@ -1,12 +1,23 @@
+import * as ShareDB from 'sharedb/lib/client';
+import * as WebSocket from 'reconnecting-websocket';
 import { createView } from '../demoView';
 import 'codemirror-6/codemirror.next/view/style/editorview.css';
 import 'codemirror-theme-ubuntu/codemirror-ubuntu-theme.css';
 import './styles.css';
 
-const path = [];
-const emitOps = ops => console.log(ops);
-const view = createView({ path, emitOps });
+const socket = new WebSocket('ws://' + window.location.host);
+const connection = new ShareDB.Connection(socket);
 
-const editorDiv = document.querySelector('#editor');
-editorDiv.innerHTML = '';
-editorDiv.appendChild(view.dom)
+const doc = connection.get('examples', 'textarea');
+
+doc.subscribe(err => {
+  if (err) {
+    throw err;
+  }
+
+  const emitOps = ops => console.log(ops);
+  const view = createView({ emitOps });
+  const editorDiv = document.querySelector('#editor');
+  editorDiv.innerHTML = '';
+  editorDiv.appendChild(view.dom)
+});
