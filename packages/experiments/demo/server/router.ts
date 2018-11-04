979f97101cb9e537fe5d3665a38ada88c372dbb8
curran <curran.kelleher@gmail.com>
2018-11-05 01:11:49 +0530

    Refactor DOM shim for per-route DOM.

--- packages/experiments/demo/server/router.ts
+++ packages/experiments/demo/server/router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { createView } from '../demoView';
-import { dom } from './dom';
+import { createDom } from './dom';
+import { globalAny } from './globalAny';
 import { hydrateEditor } from '../hydrateEditor';
 import { getOrCreateDoc } from './getOrCreateDoc';
+
+const dom = createDom();
 
 export const createRouter = connection => {
   const router = Router();
   router.get('/', (req, res) => {
     getOrCreateDoc(connection).then((doc: any) => {
+      globalAny.document = dom.window.document;
       hydrateEditor(createView({ text: doc.data }));
       res.send(dom.serialize());
       doc.destroy();
