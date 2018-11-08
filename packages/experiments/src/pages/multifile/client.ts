9df870978980e6f8270b41941472c8b9ab278950
curran <curran.kelleher@gmail.com>
2018-11-08 13:34:18 +0530

    Got multiple files demo to work. Closes #17

--- packages/experiments/src/pages/multifile/client.ts
+++ packages/experiments/src/pages/multifile/client.ts
@@ -17,25 +17,41 @@ const files = [
   }
 ];
 
-export const client = () => {
-  const text = 'hello'
-  const view = createView({ text });
+const getFile = fileName => files.find(file => file.name === fileName);
 
+const views = {};
+const getOrCreateView = selectedFile => {
+  if (!views[selectedFile.name]) {
+    const text = selectedFile.text;
+    views[selectedFile.name] = createView({ text });
+  }
+  return views[selectedFile.name];
+};
+
+const viewForFileName = fileName => getOrCreateView(getFile(fileName));
+
+export const client = () => {
+
+  let selectedFileName;
   const editorDiv = document.querySelector('#editor');
 
-  editorDiv.appendChild(view.dom);
+  const setSelectedFileName = newSelectedFileName => {
+    if (selectedFileName) {
+      const oldView = viewForFileName(selectedFileName);
+      oldView.dom.remove();
+    }
+    const newView = viewForFileName(newSelectedFileName);
+    editorDiv.appendChild(newView.dom);
 
-  const onOptionClicked = file => {
-    console.log(file);
-    editorDiv.removeChild(view.dom);
+    selectedFileName = newSelectedFileName;
   };
 
-  const options = files.map(file => file.name);
+  setSelectedFileName('index.html');
 
   select('#files-list')
     .call(dropdownMenu, {
-      options,
-      onOptionClicked,
-      selectedOption: options[0]
+      options: files.map(file => file.name),
+      onOptionClicked: setSelectedFileName,
+      selectedOption: selectedFileName
     });
 };
