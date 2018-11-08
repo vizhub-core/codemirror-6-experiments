fc031e1db7eb70256f8ae302acd97776a48ecad3
curran <curran.kelleher@gmail.com>
2018-11-08 12:52:08 +0530

    Make file menu show file names

--- packages/experiments/src/pages/multifile/client.ts
+++ packages/experiments/src/pages/multifile/client.ts
@@ -2,20 +2,40 @@ import { select } from 'd3-selection';
 import { createView } from './demoView';
 import { dropdownMenu } from './dropdownMenu';
 
+const files = [
+  {
+    name: 'index.html',
+    text: '<body><h1>Hello!</h1><script src="index.js"></body>'
+  },
+  {
+    name: 'index.js',
+    text: "import foo from './foo'; console.log(foo);"
+  },
+  {
+    name: 'foo.js',
+    text: "export default 'I am foo';"
+  }
+];
+
 export const client = () => {
   const text = 'hello'
   const view = createView({ text });
+
   const editorDiv = document.querySelector('#editor');
+
   editorDiv.appendChild(view.dom);
 
-  const options = ['a', 'b'];
+  const onOptionClicked = file => {
+    console.log(file);
+    editorDiv.removeChild(view.dom);
+  };
 
+  const options = files.map(file => file.name);
 
-  console.log('here');
   select('#files-list')
     .call(dropdownMenu, {
       options,
-      onOptionClicked: file => { console.log(file); },
+      onOptionClicked,
       selectedOption: options[0]
     });
 };
