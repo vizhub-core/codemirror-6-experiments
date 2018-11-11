6454a526e6b48716df87360442f06670c66f58f8
curran <curran.kelleher@gmail.com>
2018-11-11 17:02:36 +0530

    Sketch out tests

---
+++ packages/experiments/test/test.js
@@ -0,0 +1,12 @@
+// Pad
+//   Server render CodeMirror
+//   Type to save update
+//     Await "Saved" indicator
+//   Updates persist in pad and are server rendered
+//   Content for different documents is rendered
+//   Updates propagate between browsers
+// Multifile
+//   Switching between files shows different content
+//   Deleting a file then switching shows correct content
+// Index
+//   Content is server-rendered
