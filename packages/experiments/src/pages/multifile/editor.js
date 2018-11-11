c759de678d698f1cd3d46fabc485ad203a21edbb
curran <curran.kelleher@gmail.com>
2018-11-11 20:42:33 +0530

    Got Preact-based editor view sort of working

--- packages/experiments/src/pages/multifile/editor.js
+++ packages/experiments/src/pages/multifile/editor.js
@@ -1,24 +1,52 @@
 import { h, Component } from 'preact';
+import { createView } from './demoView';
 
 const getFile = (files, fileName) => files.find(file => file.name === fileName);
 
+const views = {};
+const getOrCreateView = selectedFile => {
+  if (!views[selectedFile.name]) {
+    const text = selectedFile.text;
+    views[selectedFile.name] = createView({ text });
+  }
+  return views[selectedFile.name];
+};
+
+const viewForFileName = (files, fileName) =>
+  getOrCreateView(getFile(files, fileName));
+
+// Technique from https://github.com/developit/preact/wiki/External-DOM-Mutations
 export class Editor extends Component {
   constructor() {
     super();
   }
 
-  render() {
-    const {
-      props: {
-        selectedFileName,
-        files
-      }
-    } = this;
+  setSelectedFileName(files, newSelectedFileName) {
+    const { selectedFileName } = this;
 
-    return (
-      <pre>
-        { getFile(files, selectedFileName).text }
-      </pre>
-    );
+    if (selectedFileName) {
+      const oldView = viewForFileName(files, selectedFileName);
+      oldView.dom.remove();
+    }
+
+    const newView = viewForFileName(files, newSelectedFileName);
+    this.base.appendChild(newView.dom);
+
+    newView.dispatch(newView.state.transaction.scrollIntoView());
+
+    this.selectedFileName = newSelectedFileName;
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { selectedFileName, files } = this.props;
+    this.setSelectedFileName(files, selectedFileName);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return <div />
   }
 }
