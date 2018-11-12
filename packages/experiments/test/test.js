caffacb4c88ecb609f920196993b0a551dbddf7b
curran <curran.kelleher@gmail.com>
2018-11-12 16:49:36 +0530

    Add test for server rendered route data

--- packages/experiments/test/test.js
+++ packages/experiments/test/test.js
@@ -15,18 +15,22 @@ const retry = (fn, ms) => new Promise(resolve => {
 });
 
 describe('vizhub-io', () => {
-  let browser;
-  let page;
 
   describe('multifilePad', () => {
+    let browser;
+    let page;
 
     it('should open page', async function() {
       browser = await puppeteer.launch(puppeteerOptions);
       page = await browser.newPage();
       const response = await retry(() => page.goto('http://localhost:3000/multifilePad'), 1000);
-      console.log(response.status());
       assert.equal(response.status(), 200);
     }).timeout(60000);
+
+    it('should render route in serverRenderedData', async function() {
+      const serverRenderedData = await page.evaluate(() => window.serverRenderedData);
+      assert.equal(serverRenderedData.route, 'multifilePad');
+    });
 
   });
 
