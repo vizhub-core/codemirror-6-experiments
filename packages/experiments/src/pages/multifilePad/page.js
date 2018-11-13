f02e85b1b98cd99467b7c155aee04ad1fce2ffc8
curran <curran.kelleher@gmail.com>
2018-11-13 18:47:08 +0530

    Access ShareDB Snapshot in page

--- packages/experiments/src/pages/multifilePad/page.js
+++ packages/experiments/src/pages/multifilePad/page.js
@@ -1,9 +1,12 @@
 import { h, Component } from 'preact';
 import { RenderingTestSentinel } from '../../renderingTestSentinel';
+import { shareDBConnection } from '../../client/shareDBConnection';
 
 export class Page extends Component {
   render(props, state) {
     const { serverRenderedData } = props;
+    const { shareDBSnapshot} = serverRenderedData;
+    console.log(shareDBSnapshot);
     return (
       <div>
         <RenderingTestSentinel />
